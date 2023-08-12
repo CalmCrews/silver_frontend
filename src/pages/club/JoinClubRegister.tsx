c789e36c2689f204c00edfebd05ecf1234da4fdd
LinQuixote <leein9404@naver.com>
2023-08-13 01:14:22 +0900

    230813 백엔드 연결전에 페이지 모두 구현

---
+++ src/pages/club/JoinClubRegister.tsx
@@ -0,0 +1,125 @@
+import React, { useState, useRef, useEffect } from "react";
+import { styled } from "styled-components";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import ClubStartBase from "../../components/club/ClubStartBase";
+import FormButton from "../../components/shared/FormButton";
+import QuestionMark from "../../assets/icons/QuestionMark.png";
+import ParticipateCodeInput from "../../components/club/ParticipateCodeInput";
+import xIcon from "../../assets/icons/xIcon.png";
+import NaverBandIcon from "../../assets/icons/NaverBandIcon.png";
+import KaKaoIcon from "../../assets/icons/KaKaoSquareIcon.png";
+
+import classes from "./MakeClubRegister.module.css";
+import InputClasses from "../../components/club/ParticipateCodeInput.module.css";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.colors.primary};
+  font-size: ${(props) => props.theme.title.md};
+  margin: 50px 0;
+  text-align: center;
+`;
+const FieldTitle = styled.p`
+  width: 100%;
+  font-size: ${(props) => props.theme.text.md};
+  font-weight: 700;
+  text-align: start;
+  color: ${(props) => props.theme.colors.black};
+  margin: 32px 0;
+  text-align: center;
+  padding-bottom: 20px;
+`;
+
+const JoinClubRegister = () => {
+  const [participateCodeNum, setParticipateCodeNum] = useState("");
+  const [isClickDesc, setIsClickDesc] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { clubName } = location.state || {};
+
+  const handleisClickDesc = (event: React.MouseEvent) => {
+    setIsClickDesc((pre) => !pre);
+  };
+
+  const handleParticipateCodeNum = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setParticipateCodeNum(event.target.value);
+  };
+
+  const handleNext = () => {
+    // 모임등록 코드 구현해야 함
+
+    // 등록한 이후 해당 모임 정보를 다음으로 보내줘야 한다
+    navigate("/club/join/profile", {
+      state: {
+        clubName: "",
+        description: "",
+      },
+    });
+  };
+
+  return (
+    <ClubStartBase>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Title>모임 참여</Title>
+        <FieldTitle>
+          기존 모임에 참여하시려면
+          <br />
+          참여코드를 입력해주세요
+        </FieldTitle>
+
+        <div className={InputClasses["outer-code-box"]}>
+          <div className={InputClasses["inner-code-desc"]}>참여코드</div>
+          <input
+            className={InputClasses["inner-code-number-input"]}
+            onChange={handleParticipateCodeNum}
+            value={participateCodeNum}
+          />
+        </div>
+
+        <div onClick={handleisClickDesc} className={classes["info-outer-div"]}>
+          <img
+            src={QuestionMark}
+            alt="물음표-아이콘"
+            className={classes["question-mark"]}
+          />
+          <div className={classes["info-inner-div"]}>참여코드가 뭐에요?</div>
+        </div>
+        {isClickDesc && (
+          <div className={classes.bubble}>
+            <span className={classes["bubble-span"]}>
+              모임마다 고유한 참여코드가 있어요!
+            </span>
+            <span className={classes["bubble-span"]}>공유하기 버튼을 눌러</span>
+            <span className={classes["bubble-span"]}>
+              모임회원들을 초대하고 코드를 알려주면
+            </span>
+            <span className={classes["bubble-span"]}>
+              모임회원들도 참여할 수 있어요!
+            </span>
+          </div>
+        )}
+        <div
+          className="button-container"
+          style={{ width: "100%", position: "absolute", bottom: "88px" }}
+        >
+          <FormButton
+            disabled={participateCodeNum.trim().length === 0}
+            onClick={handleNext}
+          >
+            모여 시작하기!
+          </FormButton>
+        </div>
+      </div>
+    </ClubStartBase>
+  );
+};
+
+export default JoinClubRegister;
--- src/pages/club/MakeClubRegister.tsx
+++ src/pages/club/MakeClubRegister.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { styled } from "styled-components";
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -6,6 +6,9 @@ import ClubStartBase from "../../components/club/ClubStartBase";
 import FormButton from "../../components/shared/FormButton";
 import QuestionMark from "../../assets/icons/QuestionMark.png";
 import ParticipateCodeInput from "../../components/club/ParticipateCodeInput";
+import xIcon from "../../assets/icons/xIcon.png";
+import NaverBandIcon from "../../assets/icons/NaverBandIcon.png";
+import KaKaoIcon from "../../assets/icons/KaKaoSquareIcon.png";
 
 import classes from "./MakeClubRegister.module.css";
 
@@ -22,47 +25,142 @@ const FieldTitle = styled.p`
   color: ${(props) => props.theme.colors.black};
   margin: 32px 0;
 `;
+const myComponentStyle = {
+  margin: "20px 0 30px 0",
+};
 
 const MakeClubRegister = () => {
-  const [] = useState();
+  // 임시로 3333 해놓음
+  const [participateCodeNum, setParticipateCodeNum] = useState("3333");
   const [isClickDesc, setIsClickDesc] = useState(false);
+  const [isClickShare, setIsClickShare] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { clubName } = location.state || {};
+
   const handleisClickDesc = (event: React.MouseEvent) => {
     setIsClickDesc((pre) => !pre);
   };
 
-  const handleCopyBtn = () => {};
-  const handleShareBtn = () => {};
+  const handleCopyBtn = () => {
+    navigator.clipboard.writeText(participateCodeNum).then(() => {
+      alert(`Copied: ${participateCodeNum}`);
+    });
+  };
+  const handleShareBtn = () => {
+    setIsClickDesc(false);
+    setIsClickShare((prev) => !prev);
+  };
+  const handleKaKaoBtn = () => {};
+  const handleBandBtn = () => {};
+  const handleLinkCopyBtn = () => {};
+
+  // 참여코드 가져오는 코드 여기에 함수로 작성해야함
+  useEffect(() => {}, []);
+
+  const handleNext = () => {};
 
   return (
     <ClubStartBase>
-      <div>모임 등록 완료!</div>
-      <div>{}</div>
-      <div>참여코드는</div>
-      <div onClick={handleisClickDesc} className={classes["info-outer-div"]}>
-        <img
-          src={QuestionMark}
-          alt="물음표-아이콘"
-          className={classes["question-mark"]}
-        />
-        <div className={classes["info-inner-div"]}>참여코드가 뭐에요?</div>
-      </div>
-      {isClickDesc && (
-        <div className={classes.bubble}>
-          <span className={classes["bubble-span"]}>
-            모임마다 고유한 참여코드가 있어요!
-          </span>
-          <span className={classes["bubble-span"]}>공유하기 버튼을 눌러</span>
-          <span className={classes["bubble-span"]}>
-            모임회원들을 초대하고 코드를 알려주면
-          </span>
-          <span className={classes["bubble-span"]}>
-            모임회원들도 참여할 수 있어요!
-          </span>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {isClickShare && (
+          <div className={classes["share-pop-up-outer-div"]}>
+            <div className={classes["share-pop-up-title"]}>
+              <img
+                src={xIcon}
+                alt="xIcon"
+                className={classes["share-pop-up-x-icon"]}
+                onClick={handleShareBtn}
+              />
+              공유하기
+            </div>
+            <div className={classes["sns-wrapper-div"]}>
+              <div
+                className={classes["sns-wrapper-outer-div "]}
+                onClick={handleKaKaoBtn}
+              >
+                <div className={classes["sns-wrapper-inner-div"]}>
+                  <img
+                    src={KaKaoIcon}
+                    alt="KaKaoIcon"
+                    className={classes["sns-icon-img"]}
+                  />
+                </div>
+                <span className={classes["sns-icon-text"]}>카카오톡</span>
+              </div>
+              <div
+                className={classes["sns-wrapper-outer-div "]}
+                onClick={handleBandBtn}
+              >
+                <div className={classes["sns-wrapper-inner-div"]}>
+                  <img
+                    src={NaverBandIcon}
+                    alt="NaverBandIcon"
+                    className={classes["sns-icon-img"]}
+                  />
+                </div>
+                <span className={classes["sns-icon-text"]}>밴드</span>
+              </div>
+            </div>
+            <div className={classes["share-pop-up-link-div"]}>
+              <span className={classes["share-pop-up-link-span"]}></span>
+              <button
+                className={classes["share-pop-up-link-btn"]}
+                onClick={handleLinkCopyBtn}
+              >
+                링크복사
+              </button>
+            </div>
+          </div>
+        )}
+        <div className={classes["sub-title"]}>모임 등록 완료!</div>
+        <div className={classes["club-title"]}>{clubName}</div>
+        <div className={classes["club-code-ment"]}>참여코드는</div>
+        <div onClick={handleisClickDesc} className={classes["info-outer-div"]}>
+          <img
+            src={QuestionMark}
+            alt="물음표-아이콘"
+            className={classes["question-mark"]}
+          />
+          <div className={classes["info-inner-div"]}>참여코드가 뭐에요?</div>
         </div>
-      )}
-      <ParticipateCodeInput clubCode={""} onClick={handleCopyBtn} />
-      <button onClick={handleShareBtn}>공유하기</button>
-      <button>모여 시작하기!</button>
+        {isClickDesc && (
+          <div className={classes.bubble}>
+            <span className={classes["bubble-span"]}>
+              모임마다 고유한 참여코드가 있어요!
+            </span>
+            <span className={classes["bubble-span"]}>공유하기 버튼을 눌러</span>
+            <span className={classes["bubble-span"]}>
+              모임회원들을 초대하고 코드를 알려주면
+            </span>
+            <span className={classes["bubble-span"]}>
+              모임회원들도 참여할 수 있어요!
+            </span>
+          </div>
+        )}
+        <ParticipateCodeInput
+          clubCode={participateCodeNum}
+          onClick={handleCopyBtn}
+          style={myComponentStyle}
+        />
+        <button onClick={handleShareBtn} className={classes["share-btn"]}>
+          공유하기
+        </button>
+        <div
+          className="button-container"
+          style={{ width: "100%", position: "absolute", bottom: "88px" }}
+        >
+          <FormButton disabled={false} onClick={handleNext}>
+            모여 시작하기!
+          </FormButton>
+        </div>
+      </div>
     </ClubStartBase>
   );
 };
