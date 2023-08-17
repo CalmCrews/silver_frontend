eeb20cdc9c81f6b44deff4ca2ecd047bdf8356e0
koomchang <leahpar0401@gmail.com>
2023-08-18 04:18:03 +0900

    feat: 잔액 충전 연결

--- src/pages/ChargeCash.tsx
+++ src/pages/ChargeCash.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useRecoilValue } from "recoil";
+import { loginState } from "../states/userInfo";
 
 interface ChargeType {
   amount: number;
@@ -7,10 +10,17 @@ interface ChargeType {
 }
 
 const ChargeCash = () => {
-  const balance: number = 100;
+  const [balance, setBalance] = React.useState<number>(0);
   const [charge, setCharge] = React.useState<number>(10000);
   const [paymethod, setPaymethod] = React.useState<string>("s");
   const [agreement, setAgreement] = React.useState<boolean>(false);
+  const user = useRecoilValue(loginState);
+  const newAxiosInstance = axios.create({
+    baseURL: "http://127.0.0.1:8000/",
+    headers: {
+      Authorization: `Bearer ${user.accessToken}`,
+    },
+  });
 
   const [chargeInfo, setChargeInfo] = React.useState<ChargeType>({
     amount: 10000,
@@ -27,9 +37,47 @@ const ChargeCash = () => {
       setCharge(cash);
     }
   };
+
   const handlePaymethod = (e: React.MouseEvent<HTMLLIElement>) => {
     setPaymethod(String(e.currentTarget.getAttribute("value")));
   };
+
+  const handlePutCharge = async () => {
+    try {
+      const response = await newAxiosInstance.put(
+        `${process.env.REACT_APP_API_URL}users/${user.user_id}/charge/`,
+        {
+          balance: charge,
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log("handlePutCharge :", error);
+    }
+  };
+  
+
+  useEffect(() => {
+    async function getMyBalance() {
+      try {
+        const response = await newAxiosInstance.get(
+          `${process.env.REACT_APP_API_URL}users/${user.user_id}/charge/`
+        );
+        return response.data;
+      } catch (error) {
+        console.log("getMyBalance inside :", error);
+        return [];
+      }
+    }
+    try {
+      getMyBalance().then((returnData) => {
+        setBalance(returnData.balance);
+      });
+    } catch (error) {
+      console.log("getMyBalance run :", error);
+    }
+  }, []);
+
 
   return (
     <>
@@ -66,7 +114,7 @@ const ChargeCash = () => {
       <div>
         <input></input> 주문할 상품의 구매조건을 확인하였으며, 결제 진행에
         동의합니다.
-        <button>충전하기</button>
+        <button onClick={handlePutCharge}>충전하기</button>
       </div>
     </>
   );
